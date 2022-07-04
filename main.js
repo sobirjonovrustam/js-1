242bb1ca1481083f71c4b4b4cae9291f08034d40
sobirjonovrustam <rustamsobirjonov33@gmail.com>
2022-07-04 23:13:57 +0500

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var name = prompt("Your name?")
+var lastName = prompt("Your last anme?")
+var age = prompt("Your age?")
+var phoneNumber = prompt("Your phone?")
+var e_mail = prompt("Your email?")
+var adress = prompt("Your adress?")
+var work = prompt("Your workplace?")
+var married = prompt("Are you married?")
+var hobby = prompt("Your hobby?")
+var liked = prompt("Do you remember our site?", "yes")
+
+console.log(`Your name: ${name}, \nYour last name: ${lastName}, \nYour age: ${age}, \nYour phone: ${phoneNumber}, \nYour email: ${e_mail}, \nYour adress: ${adress}, \nYour workplase: ${work}, \nAre you married: ${married}, \nYour hobby: ${hobby}, \nDo you remember our site: ${liked}`);
+
+alert(`Your name: ${name}, \nYour last name: ${lastName}, \nYour age: ${age}, \nYour phone: ${phoneNumber}, \nYour email: ${e_mail}, \nYour adress: ${adress}, \nYour workplase: ${work}, \nAre you married: ${married}, \nYour hobby: ${hobby}, \nDo you remember our site: ${liked}`);
